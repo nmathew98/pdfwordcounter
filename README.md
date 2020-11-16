308c5354623d4b3e177209ec4ef56e7ef306283e
Naveen Mathew <naveenmat98+github@gmail.com>
2020-11-16 20:17:50 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pdfwordcounter - Count the number of times words occur in a PDF
+
+Simple tool in Python I made for a friend. Takes in some words and prints out the number of times the word occurs in a PDF.
---
+++ main.py
@@ -0,0 +1,32 @@
+import pdftotext
+import os
+import datetime
+
+# Find all PDFs in directory
+files = []
+for file in os.listdir("pdfs"):
+	if file.endswith(".pdf"):
+		files.append(os.path.join("pdfs", file))
+
+# Words we are looking for
+keys = []
+with open("search.txt", "r") as f:
+	for line in f:
+		keys.extend(line.strip().split("\n"))
+
+for file in files:
+	# Create a dictionary with the keys we are looking for
+	dictionary = {key: 0 for key in keys}
+	with open(file, "rb") as f:
+		pdf = pdftotext.PDF(f)
+
+	for page in pdf:
+		for key in keys:
+			dictionary[key] += page.lower().strip().count(key.lower())
+
+	# Print occurences to a file
+	with open('occurences.txt', 'a') as f:
+		print(f"Filename: {file} [{len(pdf)} pages]; Date: {datetime.date.today()}", file=f)
+		for k, v in dictionary.items():
+			print(f"{k}: {v}", file=f)
+		print("", file=f)
